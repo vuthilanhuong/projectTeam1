22a9ce12004b6eafc32c3aee05f3c60545a189e9
hsang28999 <sanghd00541@fpt.edu.vn>
2018-01-17 17:27:42 +0700

    demo-17/1/2017

---
+++ clientSide/Admin/js/app.js
@@ -0,0 +1,702 @@
+var app = angular.module('myApp',['ui.router', 'ui.bootstrap']);
+
+app.config(appRouterConfig);
+function appRouterConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+			.state('Product', {
+				url: '/product/page/:pageID',
+				templateUrl: 'pages/product/listProduct.html',
+				controller: 'productCtrl'
+			})
+			.state('AddProduct', {
+				url: '/product/add',
+				templateUrl: 'pages/product/addProduct.html',
+				controller: 'addProductCtrl'
+			})
+			.state('EditProduct', {
+				url: '/product/:productID/edit',
+				templateUrl: 'pages/product/editProduct.html',
+				controller: 'editProductCtrl'
+			}).state('User', {
+				url: '/user/page/:pageID',
+				templateUrl: 'pages/user/listUser.html',
+				controller: 'userCtrl'
+			}).state('AddUser', {
+				url: '/user/add',
+				templateUrl: 'pages/user/addUser.html',
+				controller: 'addUserCtrl'
+			}).state('EditUser', {
+				url: '/user/:userID/edit',
+				templateUrl: 'pages/user/editUser.html',
+				controller: 'editUserCtrl'
+			}).state('Admin', {
+				url: '/admin/page/:pageID',
+				templateUrl: 'pages/admin/listAdmin.html',
+				controller: 'adminCtrl'
+			}).state('AddAdmin', {
+				url: '/admin/add',
+				templateUrl: 'pages/admin/addAdmin.html',
+				controller: 'addAdminCtrl'
+			}).state('EditAdmin', {
+				url: '/admin/:userID/edit',
+				templateUrl: 'pages/admin/editAdmin.html',
+				controller: 'editAdminCtrl'
+			});
+	
+		$urlRouterProvider.otherwise('/product/page/1'); 
+};
+
+app.controller('indexCtrl',function($scope,$http){
+	if(localStorage.admin === undefined){
+		window.location.href = "pages/auth/login.html";
+	};
+	var secret = JSON.parse(localStorage.admin);
+	console.log(secret.userID);
+	console.log(secret.token);
+	$scope.loadAdmin = function(){
+		$http({
+			method: 'GET',
+			headers: {
+				"Authorization": secret.token
+			},
+			url: 'http://localhost:3000/_api/v1/getAdmin/' + secret.userID
+		}).then(function mySuccess(response){
+    		if(response.data === null)
+    		window.location.href = "pages/auth/login.html";
+    		$scope.admin = response.data;
+    	}, function myError(response) {
+    		console.log('Error authenication');
+    		window.location.href = "pages/auth/login.html";
+    	});
+	};
+
+	$scope.logout = function () {
+    	if(confirm("Are you sure?")){
+            localStorage.removeItem("admin");
+            window.location.href = "pages/auth/login.html";		
+        }
+    };
+});
+
+app.controller('productCtrl',function($scope,$stateParams,$http){
+	var idxPage = $stateParams.pageID;
+
+	$scope.loadProduct = function() {
+	    $http({
+			method: 'GET',
+			url: 'http://localhost:3000/_api/v1/products' + '?page=' + idxPage
+		}).then(function mySuccess(response){
+    		console.log(response);
+    		$scope.restored_data = response.data;
+            console.log('Get thanh cong');
+    	}, function myError(response) {
+    		console.log('loi dang ky');
+    	});
+	    $scope.currentPage = idxPage;
+	};
+
+	$scope.edit = function(item){
+		localStorage.editProduct = JSON.stringify(item);
+	};
+
+	$scope.remove = function(item){ 
+        $http({
+			method: 'DELETE',
+			url: 'http://localhost:3000/_api/v1/products/' + item._id
+		}).then(function mySuccess(response){
+    		console.log(response);
+            console.log('Delete thanh cong');
+            window.location.reload();
+    	}, function myError(response) {
+    		console.log('Delete fail');
+    	});   
+    };
+});
+//                                 >>>>>>>>>>>>>>>>>>>Product<<<<<<<<<<<<<<<<<<<   
+
+app.controller('editProductCtrl',function($scope,$http){
+
+	$scope.loadFix = function(){
+		$scope.detail_Product = JSON.parse(localStorage.editProduct);
+		$scope.ProductType = ["Kính Thể Thao","Kính Áp Tròng","Kính Mát Nữ","Kính Mát Nam","Kính Mát Trẻ Em"];
+		$scope.Brand = ["Ray-Ban","Oakley","Dolce & Gabbana","Burberry","Versace"];
+		$scope.Availability = ["Có Sẵn Hàng","Hết Hàng"];
+	};
+
+	$scope.submitFix = function(data){
+		$http({
+			method: 'PUT',
+			url: 'http://localhost:3000/_api/v1/products/' + data._id,
+			data: data
+		}).then(function mySuccess(response){
+    		swal({
+			  title: "Thành Công!",
+			  text: " ",
+			  icon: "success",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	}, function myError(response) {
+    		swal({
+			  title: "Thất Bại!",
+			  text: " ",
+			  icon: "error",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	});
+	};
+
+	$scope.uploading1 = function(){
+		var fileUpload = document.getElementById("fileUpload1");
+		var fd = new FormData();
+			fd.append('file', fileUpload.files[0]);
+			fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			$scope.detail_Product.Picture1 = response.data.secure_url;
+    	}, function myError(response) {
+    		console.log('That Bai')
+	    })
+	};
+
+	$scope.uploading2 = function(){
+		var fileUpload = document.getElementById("fileUpload2");
+		var fd = new FormData();
+			fd.append('file', fileUpload.files[0]);
+			fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.detail_Product.Picture1 = response.data.secure_url;					
+            console.log('thanh cong upload');
+    	}, function myError(response) {
+    		console.log('loi upload');
+    		console.log(response);
+	    })
+	};
+});
+
+
+app.controller('addProductCtrl',function($scope,$http){
+	var smallImg = [];
+	$scope.ProductType = ["Kính Thể Thao","Kính Áp Tròng","Kính Mát Nữ","Kính Mát Nam","Kính Mát Trẻ Em"];
+	$scope.Brand = ["Ray-Ban","Oakley","Dolce & Gabbana","Burberry","Versace"];
+	$scope.Availability = ["Có Sẵn Hàng","Hết Hàng"];
+
+	//upload anh input
+	$scope.uploading1 = function(){
+		var fileUpload = document.getElementById("fileUpload1");
+		var fd = new FormData();
+			fd.append('file', fileUpload.files[0]);
+			fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.Picture1 = response.data.secure_url;
+			$scope.Picture1Show = true;
+            console.log('thanh cong upload');
+
+    	}, function myError(response) {
+    		console.log('loi upload');
+    		console.log(response);
+	    })
+	};
+
+	$scope.uploading2 = function(){
+		var fileUpload = document.getElementById("fileUpload2");
+		var fd = new FormData();
+			fd.append('file', fileUpload.files[0]);
+			fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.Picture2 = response.data.secure_url;					
+            console.log('thanh cong upload');
+            $scope.Picture2Show = true;
+    	}, function myError(response) {
+    		console.log('loi upload');
+    		console.log(response);
+	    })
+	};
+	//end upload
+
+	//submit form
+	$scope.save = function(){
+		
+		//post product
+		var sendData = $scope.data;
+		sendData.Picture1 = $scope.Picture1;
+		sendData.Picture2 = $scope.Picture2;
+		console.log($scope.data);
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/_api/v1/products',
+			data: sendData
+		}).then(function mySuccess(response){
+    		swal({
+			  title: "Thành Công!",
+			  text: " ",
+			  icon: "success",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	}, function myError(response) {
+    		swal({
+			  title: "Thất Bại!",
+			  text: "Sản phẩm đã tồn tại",
+			  icon: "error",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	});
+		//end post product
+	};
+	//end submit
+
+	//delete img upload
+	$scope.deleteImg = function(img){
+		var index=$scope.smallImg.indexOf(img);
+        if(confirm("Bạn có chắc chắn xóa không?")){
+            $scope.smallImg.splice(index,1);  
+        }
+	};
+	//end delete img upload
+});
+
+//                                 >>>>>>>>>>>>>>>>>>>User<<<<<<<<<<<<<<<<<<<   
+
+app.controller("userCtrl",function($scope,$stateParams,$http){
+	var idxPage = $stateParams.pageID;
+
+	$scope.loadUser = function() {
+	    $http({
+			method: 'GET',
+			url: 'http://localhost:3000/_api/v1/members/' + '?page=' + idxPage
+		}).then(function mySuccess(response){
+    		console.log(response);
+    		$scope.restored_data = response.data;
+            console.log('Get thanh cong');
+    	}, function myError(response) {
+    		console.log('loi dang ky');
+    	});
+	    $scope.currentPage = idxPage;
+	};
+
+	$scope.edit = function(item){
+		localStorage.editUser = JSON.stringify(item);
+	};
+
+	$scope.remove = function(item){ 
+        $http({
+			method: 'DELETE',
+			url: 'http://localhost:3000/_api/v1/members/' + item._id
+		}).then(function mySuccess(response){
+    		console.log(response);
+            console.log('Delete thanh cong');
+            window.location.reload();
+    	}, function myError(response) {
+    		console.log('Delete that bai');
+    	});   
+    };
+});
+
+app.controller("addUserCtrl",function($scope,$http){
+	$scope.gender = [{name:"Nam",value:"1"},{name:"Nữ",value:"2"}];
+	$scope.previewShow = true;
+
+	$scope.uploading = function(){
+		var fileUpload = document.getElementById("fileUpload");
+		var fd = new FormData();
+		fd.append('file', fileUpload.files[0]);
+		fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.img = response.data.secure_url;					
+            console.log('thanh cong upload');
+            $scope.previewShow = false;
+            $scope.uploadErr = false
+    	}, function myError(response) {
+    		console.log('loi upload');
+    		console.log(response);
+	    });
+	};
+
+	$scope.submitForm = function(){
+		var data = $scope.sendData;
+			data.gender = data.gender.value;
+			data.avatarUrl = $scope.img;
+			console.log(data);
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/_api/v1/members',
+			data: data
+		}).then(function mySuccess(response){
+    		swal({
+			  title: "Thành Công!",
+			  text: " ",
+			  icon: "success",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	}, function myError(response) {
+    		swal({
+			  title: "Thất Bại!",
+			  text: " ",
+			  icon: "error",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	}); 
+	};
+
+	$scope.resetForm = function(){
+		$scope.previewShow = true
+	};
+
+	//Error messages
+	$scope.userError = function(){
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/_api/v1/checkUser',
+			data : $scope.sendData
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.checkUser = true
+    	}, function myError(response) {
+    		$scope.checkUser = false
+	    });
+	};
+
+	$scope.resetUser = function(){
+		$scope.checkUser = false
+	};
+
+	$scope.confirmError = function(){
+		if ($scope.confirmPassword == $scope.sendData.password) {
+			$scope.checkConfirm = false;
+		}else{
+			$scope.checkConfirm = true;
+		}			
+	};
+
+	$scope.resetConfirm = function(){
+		$scope.checkConfirm = false
+	};
+
+	$scope.emailError = function(){
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/_api/v1/checkEmail',
+			data: $scope.sendData
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.checkEmail = true
+    	}, function myError(response) {
+    		$scope.checkEmail = false
+	    });		
+	};
+
+	$scope.resetEmail = function(){
+		$scope.checkEmail = false
+	};
+
+	$scope.birthDayError = function(){
+		var today = new Date();
+		var condition = today.setFullYear(today.getFullYear()-3)
+
+		if ($scope.sendData.birthDay < condition) {
+			$scope.checkBirthDay = false;
+		}else{
+			$scope.checkBirthDay = true;
+		}	
+	};
+});
+
+app.controller('editUserCtrl',function($scope,$http){
+
+	$scope.loadFix = function(){
+		$scope.detail_user = JSON.parse(localStorage.editUser);
+		$scope.detail_user.birthDay = new Date($scope.detail_user.birthDay);
+		$scope.gender = [{name:"Nam",value:"1"},{name:"Nữ",value:"2"}];
+	};
+
+	$scope.submitFix = function(data){
+		data.avatarUrl = $scope.img;
+		$http({
+			method: 'PUT',
+			url: 'http://localhost:3000/_api/v1/members/' + data._id,
+			data: data
+		}).then(function mySuccess(response){
+    		console.log(response);
+            console.log('Update thanh cong');
+    	}, function myError(response) {
+    		console.log('Update loi');
+    	});
+	};
+
+	$scope.uploading = function(){
+		var fileUpload = document.getElementById("fileUpload");
+		var fd = new FormData();
+		fd.append('file', fileUpload.files[0]);
+		fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.detail_user.avatarUrl = response.data.secure_url;					
+            console.log('thanh cong upload');
+    	}, function myError(response) {
+    		console.log('loi upload');
+    		console.log(response);
+	    });
+	};
+
+});
+
+//                               >>>>>>>>>>>>>>>>>>>ADMIN<<<<<<<<<<<<<<<<<<<   
+
+app.controller("adminCtrl",function($scope,$stateParams,$http){
+	var idxPage = $stateParams.pageID;
+
+	$scope.loadAdmin = function() {
+	    $http({
+			method: 'GET',
+			url: 'http://localhost:3000/_api/v1/admins/' + '?page=' + idxPage
+		}).then(function mySuccess(response){
+    		console.log(response);
+    		$scope.restored_data = response.data;
+            console.log('Get thanh cong');
+    	}, function myError(response) {
+    		console.log('loi dang ky');
+    	});
+	    $scope.currentPage = idxPage;
+	};
+
+	$scope.edit = function(item){
+		localStorage.editAdmin = JSON.stringify(item);
+	};
+
+	$scope.remove = function(item){ 
+        $http({
+			method: 'DELETE',
+			url: 'http://localhost:3000/_api/v1/admins/' + item._id
+		}).then(function mySuccess(response){
+    		console.log(response);
+            console.log('Delete thanh cong');
+            window.location.reload();
+    	}, function myError(response) {
+    		console.log('Delete that bai');
+    	});   
+    };
+});
+
+app.controller("addAdminCtrl",function($scope,$http){
+	$scope.gender = [{name:"Nam",value:"1"},{name:"Nữ",value:"2"}];
+	$scope.previewShow = true;
+
+	$scope.uploading = function(){
+		var fileUpload = document.getElementById("fileUpload");
+		var fd = new FormData();
+		fd.append('file', fileUpload.files[0]);
+		fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.img = response.data.secure_url;					
+            console.log('thanh cong upload');
+            $scope.previewShow = false;
+            $scope.uploadErr = false
+    	}, function myError(response) {
+    		console.log('loi upload');
+    		console.log(response);
+	    });
+	};
+
+	$scope.submitForm = function(){
+		var data = $scope.sendData;
+			data.birthDay = $scope.birthDay;
+			data.gender = data.gender.value;
+			data.avatarUrl = $scope.img;
+			console.log(data);
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/_api/v1/admins',
+			data: data
+		}).then(function mySuccess(response){
+    		swal({
+			  title: "Thành Công!",
+			  text: " ",
+			  icon: "success",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	}, function myError(response) {
+    		swal({
+			  title: "Thất Bại!",
+			  text: " ",
+			  icon: "error",
+			  timer: 1000,
+  			  buttons: false
+			})
+    	}); 
+	};
+
+	$scope.resetForm = function(){
+		$scope.previewShow = true
+	};
+
+	//Error messages
+	$scope.userError = function(){
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/_api/v1/admin/checkUser',
+			data : $scope.sendData
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.checkUser = true
+    	}, function myError(response) {
+    		$scope.checkUser = false
+	    });
+	};
+
+	$scope.resetUser = function(){
+		$scope.checkUser = false
+	};
+
+	$scope.confirmError = function(){
+		if ($scope.confirmPassword == $scope.sendData.password) {
+			$scope.checkConfirm = false;
+		}else{
+			$scope.checkConfirm = true;
+		}			
+	};
+
+	$scope.resetConfirm = function(){
+		$scope.checkConfirm = false
+	};
+
+	$scope.emailError = function(){
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/_api/v1/admin/checkEmail',
+			data: $scope.sendData
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.checkEmail = true
+    	}, function myError(response) {
+    		$scope.checkEmail = false
+	    });		
+	};
+
+	$scope.resetEmail = function(){
+		$scope.checkEmail = false
+	};
+
+	$scope.birthDayError = function(){
+		var today = new Date();
+		var condition = today.setFullYear(today.getFullYear()-3)
+
+		if ($scope.sendData.birthDay < condition) {
+			$scope.checkBirthDay = false;
+		}else{
+			$scope.checkBirthDay = true;
+		}	
+	};
+});
+
+app.controller('editAdminCtrl',function($scope,$http){
+
+	$scope.loadFix = function(){
+		$scope.detail_admin = JSON.parse(localStorage.editAdmin);
+		$scope.detail_admin.birthDay = new Date($scope.detail_admin.birthDay);
+		$scope.gender = [{name:"Nam",value:"1"},{name:"Nữ",value:"2"}];
+	};
+
+	$scope.submitFix = function(data){
+		data.avatarUrl = $scope.img;
+		$http({
+			method: 'PUT',
+			url: 'http://localhost:3000/_api/v1/admins/' + data._id,
+			data: data
+		}).then(function mySuccess(response){
+    		console.log(response);
+            console.log('Update thanh cong');
+    	}, function myError(response) {
+    		console.log('Update loi');
+    	});
+	};
+
+	$scope.uploading = function(){
+		var fileUpload = document.getElementById("fileUpload");
+		var fd = new FormData();
+		fd.append('file', fileUpload.files[0]);
+		fd.append('upload_preset','rk1gl1ni');
+		$http({
+			method: 'POST',
+			url: 'https://api.cloudinary.com/v1_1/sangbeo-aptech/image/upload',
+			headers:{"Content-Type": undefined},
+			data: fd,
+			cache: false,
+		    contentType: false,
+		    processData: false	
+		}).then(function mySuccess(response){
+			console.log(response);
+			$scope.detail_user.avatarUrl = response.data.secure_url;					
+            console.log('thanh cong upload');
+    	}, function myError(response) {
+    		console.log('loi upload');
+    		console.log(response);
+	    });
+	};
+
+});
---
+++ serverSide/Project ky1/api/controllers/productController.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+var Product = require('../models/Product');
+require('mongoose-pagination');
+
+exports.get_list = function(req, resp){
+	console.log('i am getting list product.');
+	var page = Number(req.query.page);
+	var limit = 10;
+	Product.find({status:1}).paginate(page, limit, function(err, result, total) {
+    	console.log('total: ', total, 'result: ', result);
+    	var responseData = {
+    		'listProduct': result,
+    		'total': total
+    	};
+    	resp.send(responseData);
+  	});
+};
+
+exports.add = function(req, resp){
+	console.log('i am adding product.');
+	var product = new Product(req.body);
+	Product.on('index', function (error) {
+		if (error)
+	    	resp.send(error);
+	});
+	product.save(function (err) {
+	    if (err) {
+	    console.log(err);
+	    resp.status(500);
+	    resp.send('Name product already exists');
+	    } else {
+	    resp.send('Save success');
+	    }
+	})
+};
+
+exports.get_detail = function(req, resp){
+	console.log('i am getting a product.');
+	Product.findById(req.params.id, function(err, result) {
+    	if (err)
+      	  resp.send(err);
+    	resp.json(result);
+  	});
+
+};
+
+exports.update = function(req, resp){
+	console.log('i am updating product.');
+	Product.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, task) {
+    	if (err)
+      	  resp.send(err);
+    	resp.json(task);
+  	});
+};
+
+exports.delete = function(req, resp){
+	console.log('i am deleting product.');
+	Product.findOneAndUpdate({_id: req.params.id}, {"status": 0}, {new: true}, function(err, task) {
+    	if (err)
+      	  resp.send(err);
+    	resp.json(task);
+  	});
+};
+
