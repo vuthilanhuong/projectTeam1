5453b9c3c4ad9b08b26c0ac190877656ba680eb8
hsang28999 <sanghd00541@fpt.edu.vn>
2018-01-25 14:35:36 +0700

    sangcucdeptrai

--- Server-Side/api/models/order_detail.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-module.exports = mongoose.model('order_details', {	
-	orderId: Schema.Types.ObjectId,
-	productId: Schema.Types.ObjectId,
-	quantity: Number,	
-	unitPrice: Number
-});
