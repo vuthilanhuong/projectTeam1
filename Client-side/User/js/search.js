59da93e6dc6923f0d9962f84dde70f5cd0f6e4f8
hsang28999 <sanghd00541@fpt.edu.vn>
2018-01-29 17:53:44 +0700

    delete old

--- Client-side/User/js/search.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('form#searchItem').submit(function(){
-    	var key = $('#search').val();
-    	window.location.href = 'product.html?search='+key;
-    });
-});
